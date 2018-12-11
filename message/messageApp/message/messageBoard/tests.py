f9b653ebc6535b19d713a37c24d712ffd4f91258
SarpongAbasimi <37377831+SarpongAbasimi@users.noreply.github.com>
2018-12-11 15:12:39 +0000

    Update tests.py

--- message/messageApp/message/messageBoard/tests.py
+++ message/messageApp/message/messageBoard/tests.py
@@ -2,22 +2,29 @@ from django.test import TestCase
 
 from django.test import Client
 from .models import Posts
+from django.urls import reverse
 
 # Create your tests here.
-""" Testing the Home View """
-
-class SimpleTest(TestCase):
+class HomePageViewTest(TestCase):
 
     def setUp(self):
         self.client=Client()
         
-    def test_home_view(self):
+    def test_home_view_exit_at_location(self):
         response=self.client.get('/')
-
         self.assertEqual(response.status_code,200)
     
-  """ Testinf the Posts model"""  
-  
+    def test_view_url_by_name(self):
+        response=self.client.get(reverse('home_page'))
+        self.assertEqual(response.status_code,200)
+
+    def test_view_uses_correct_template(self):
+        response=self.client.get('/')
+        self.assertEqual(response.status_code,200)
+        self.assertTemplateUsed(response,'home.html')
+
+    
+    
 class PostModelTest(TestCase):
     def setUp(self):
         self.mytest='just test'
@@ -27,4 +34,3 @@ class PostModelTest(TestCase):
         post=Posts.objects.get(id=1)
         expected_text_contnent= f"{post.text}"
         self.assertEqual(expected_text_contnent,self.mytest)
-
